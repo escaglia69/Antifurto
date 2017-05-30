4f05ed613e90335681980cb98c655df38b3977e2
Emanuele Scaglia <escaglia69@yahoo.com>
2017-05-31 00:05:04 +0200

    Used DS1307RTC library instead of RTClib

--- basemegaesp/basemegaesp.ino
+++ basemegaesp/basemegaesp.ino
@@ -1,19 +1,17 @@
-#define BLYNK_PRINT Serial
+//#define BLYNK_PRINT Serial
 #include "RF24.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include "RTClib.h"
 #include <EEPROM.h>
 #include <ESP8266_Lib.h>
 #include "BlynkSimpleShieldEsp8266.h"
+#include <DS1307RTC.h>
 #include <TimeLib.h>
 #include <WidgetRTC.h>
 #include "secret.h"
 
 bool isFirstConnect = true;
 WidgetLCD wlcd(V5);
-
-RTC_DS1307 rtc;
 WidgetRTC brtc;
 
 RF24 radio(5,9);
@@ -25,7 +23,7 @@ struct sensorDataRecord {
   boolean intDoor;
   boolean extDoor;
   float vcc;
-  String dateTime;
+  char dateTime[20];
 };
 const int SENSOR_NUM = 14;
 sensorDataRecord sensorData[SENSOR_NUM] = {{0,0.0,1,1,0.0,"01/01/2016 00:00:00"},{1,0.0,1,1,0.0,"01/01/2016 00:00:00"},{2,0.0,1,1,0.0,"01/01/2016 00:00:00"},{3,0.0,1,1,0.0,"01/01/2016 00:00:00"},{4,0.0,1,1,0.0,"01/01/2016 00:00:00"},{5,0.0,1,1,0.0,"01/01/2016 00:00:00"},{6,0.0,1,1,0.0,"01/01/2016 00:00:00"},{7,0.0,1,1,0.0,"01/01/2016 00:00:00"},{8,0.0,1,1,0.0,"01/01/2016 00:00:00"},{9,0.0,1,1,0.0,"01/01/2016 00:00:00"},{10,0.0,1,1,0.0,"01/01/2016 00:00:00"},{11,0.0,1,1,0.0,"01/01/2016 00:00:00"},{12,0.0,1,1,0.0,"01/01/2016 00:00:00"},{13,0.0,1,1,0.0,"01/01/2016 00:00:00"}};
@@ -74,6 +72,7 @@ char tmpid[2] = "";
 uint8_t jsonline[128];
 int jsonlinel = 0;
 int reslen = 0;
+tmElements_t tm;
 
 #define EspSerial Serial3
 #define ESP8266_BAUD 115200
@@ -81,42 +80,29 @@ int reslen = 0;
 ESP8266 wifi(&EspSerial);
 BlynkTimer timer;
 
-/*char timeServer[] = "time.nist.gov";  // NTP server
-unsigned int localPort = 2390;        // local port to listen for UDP packets
-const int NTP_PACKET_SIZE = 48;  // NTP timestamp is in the first 48 bytes of the message
-const int UDP_TIMEOUT = 2000;    // timeout in miliseconds to wait for an UDP packet to arrive
-byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets*/
-
 void setup(void){
   Serial.begin(115200);
   Serial.println("Setup!");
-  //delay(10);
-  //setAllIntEnabled();
-  //getAllEnabled();
-  //ApplicationMonitor.Dump(Serial);
-  //ApplicationMonitor.EnableWatchdog(Watchdog::CApplicationMonitor::Timeout_8s);
   lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
   lcd.noBacklight(); // finish with backlight on
   lcd.setCursor(0,0); //Start at character 4 on line 0
   //lcd.print(F("Allarme 1.0"));
   //lcd.setCursor(0,1);
   lcd.print(F("Avviato!"));
-  //delay(30);  
-  rtc.begin();
-  //delay(30);
-  if (! rtc.isrunning()) {
-    Serial.println(F("RTC is NOT running!"));
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(__DATE__, __TIME__));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  if (RTC.read(tm)) {
+    sprintf(startedAt, "%02d/%02d/%04d %02d:%02d:%02d", tm.Day, tm.Month, tmYearToCalendar(tm.Year), tm.Hour, tm.Minute, tm.Second);
+    Serial.print("Started at: ");
+    Serial.println(startedAt);    
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+    delay(100);
   }
-  //delay(30);
-  DateTime now = rtc.now();
-  sprintf(startedAt, "%02d/%02d/%04d %02d:%02d:%02d", now.day(), now.month(), now.year(), now.hour(), now.minute(), now.second());
-  Serial.print("Started at: ");
-  Serial.println(startedAt);
+  delay(100);
   
   pinMode(highButtonPin, INPUT);
   digitalWrite(highButtonPin, HIGH);
@@ -161,65 +147,19 @@ void setup(void){
       Serial.print("set tcp server timout err\r\n");
   }
 
-  /*sendNTPpacket(&wifi, timeServer); // send an NTP packet to a time server
-  // wait for a reply for UDP_TIMEOUT miliseconds
-  unsigned long startMs = millis();
-  //while (!wifi.available() && (millis() - startMs) < UDP_TIMEOUT) {}
-
-  //Serial.println(Udp.parsePacket());
-  //if (Udp.parsePacket()) {
-    Serial.println("packet received");
-    // We've received a packet, read the data from it into the buffer
-    wifi.recv(packetBuffer, NTP_PACKET_SIZE);
-
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if (((epoch % 3600) / 60) < 10) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ((epoch % 60) < 10) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  //}
-  // wait ten seconds before asking for the time again
-  delay(10000);*/
-
 }
 
 void CheckConnection(){
-  rtc.adjust(DateTime(year(), month(), day(), hour(), minute(), second()));
+  tm.Year = CalendarYrToTm(year());
+  tm.Month = month();
+  tm.Day = day();
+  tm.Hour = hour();
+  tm.Minute = minute();
+  tm.Second = second();
+  RTC.write(tm);
   Connected2Blynk = Blynk.connected();
   if(!Connected2Blynk){
     Serial.println(F("Not connected"));
-    //Blynk.connect(10000);  // timeout set to 10 seconds and then continue without Blynk
     Blynk.reconnect(ssid, pass);
   }
 }
@@ -397,14 +337,15 @@ void resetAlarm() {
 }
 
 void copySensorData(int sid) {
-  DateTime now = rtc.now();
+  //DateTime now = rtc.now();
+  RTC.read(tm);
   sensorData[sid].temp = tempData.temp;
   sensorData[sid].intDoor = tempData.intDoor;
   sensorData[sid].extDoor = tempData.extDoor;
   sensorData[sid].vcc = tempData.vcc;
   char buffer [20] = "";
-  sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", now.day(), now.month(), now.year(), now.hour(), now.minute(), now.second());
-  sensorData[sid].dateTime = buffer;
+  sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tm.Day, tm.Month, tmYearToCalendar(tm.Year), tm.Hour, tm.Minute, tm.Second);
+  strcpy(sensorData[sid].dateTime,buffer);
   //delay(5);
 }
 
@@ -533,6 +474,7 @@ BLYNK_CONNECTED() // runs every time Blynk connection is established
       isFirstConnect = false;
     }
     wlcd.clear();
+    //clockDisplay();
 }
 
 BLYNK_WRITE(V0) {
@@ -578,30 +520,6 @@ BLYNK_WRITE(V4) {
   }
 }
 
-/*void sendNTPpacket(ESP8266* client, String ntpSrv)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  client->registerUDP(ntpSrv, 123); //NTP requests are to port 123
-  client->send(packetBuffer, NTP_PACKET_SIZE);
-  client->unregisterUDP();
-}*/
-
 void clockDisplay()
 {
   // You can call hour(), minute(), ... at any time
@@ -609,11 +527,11 @@ void clockDisplay()
 
   String currentTime = String(hour()) + ":" + minute() + ":" + second();
   String currentDate = String(day()) + " " + month() + " " + year();
-  Serial.print("Current time: ");
+  /*Serial.print("Current time: ");
   Serial.print(currentTime);
   Serial.print(" ");
   Serial.print(currentDate);
-  Serial.println();
+  Serial.println();*/
 
   // Send time to the App
   Blynk.virtualWrite(V1, currentTime);
@@ -641,7 +559,7 @@ int printToJSON(uint8_t (*row)[128], int sid) {
     memcpy(*row+58,"\", \"Battery V\":\"",16);
     memcpy(*row+74,vcc,4);
     memcpy(*row+78,"\", \"Read time\":\"",16);
-    memcpy(*row+94,sensorData[sid].dateTime.c_str(),19);
+    memcpy(*row+94,sensorData[sid].dateTime,19);
     if (sid == SENSOR_NUM-1 ) {
       memcpy(*row+113,"\"}\n",3);
       return 116;
@@ -707,7 +625,8 @@ void http_process(ESP8266* client, uint8_t mux_id, uint32_t len) {
               memcpy(msg+197,"intDisable?id=\n",15);
               memcpy(msg+212,"extEnable?id=\n",14);
               memcpy(msg+226,"extDisable?id=\n",15);
-              reslen=241;
+              memcpy(msg+241,"sync\n",5);
+              reslen=246;
             } else if (readString.startsWith(F("GET /arm"))) {
               arm();
               memcpy(msg+91,"Armed\n",6);
@@ -738,6 +657,10 @@ void http_process(ESP8266* client, uint8_t mux_id, uint32_t len) {
               reslen=113;
             } else if (readString.startsWith(F("GET /printEnabled"))) {
               reslen=printAllEnabled(&msg);
+            } else if (readString.startsWith(F("GET /sync"))) {
+              clockDisplay();
+              memcpy(msg+91,"Time synced\n",12);
+              reslen=103;
             } else if (readString.startsWith(F("GET /intEnable?id="))) {
               int id = getId(18);
               if ((id >=0) && (id < SENSOR_NUM)) {
